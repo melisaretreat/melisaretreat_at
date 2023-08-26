6c5de0841e82b0c210e32a9eed7738a7300ed3bb
Raphael Krupinski <10319569-mattesilver@users.noreply.gitlab.com>
2023-08-26 21:13:53 +0200

    Uninline styles

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -11,6 +11,7 @@ const config: GatsbyConfig = {
     graphqlTypegen: true,
     plugins: [
         "gatsby-plugin-postcss",
+        'gatsby-plugin-uninline-styles',
         "gatsby-plugin-image",
         "gatsby-plugin-sitemap",
         {
